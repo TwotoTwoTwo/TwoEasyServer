504d9c1a7b1053cfe41de68edf9e8c178deea8e2
wsjiu <527172036@qq.com>
2020-10-30 17:19:35 +0800

    服务端代码初始化

---
+++ src/main/java/cn/wsjiu/App.java
@@ -0,0 +1,11 @@
+package cn.wsjiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App extends SpringApplication {
+    public static void main(String[] args) {
+        run(App.class);
+    }
+}
